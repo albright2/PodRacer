c887e64a5f0ef0678743fe68fc91f2c2aaf918d8
Jeremiah Albright <43688937+albright2@users.noreply.github.com>
2019-05-22 11:13:11 -0700

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,4 @@
+javac a3\MyGame.java
+pause
+java  a3.MyGame 192.168.0.88 9877
+pause
---
+++ runserver.bat
@@ -0,0 +1,4 @@
+javac Networking\NetworkingServer.java
+pause
+java -Dsun.java2d.d3d=false Networking.NetworkingServer 9877 UDP
+pause
